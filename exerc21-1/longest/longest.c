4bf8e92647eb028ef49837b40686e849678507d5
alexece07 <80840634+alexece07@users.noreply.github.com>
2021-03-29 15:19:03 +0300

    Update longest.c

--- exerc21-1/longest/longest.c
+++ exerc21-1/longest/longest.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
 
-int main(argc, argv[]){
+int main(int argc,char *argv[]){
+    FILE *f;
+    if(f = fopen(argv[1],"r")== NULL) return 1;
+    else {
+       int M,N,sum=0,day;
+       fscanf(f,"%d",&M);
+       fscanf(f,"%d",&N);
+       int A[1000000], B[1000000];
+       while(fgetc(f)!="\n");
+       for(int i=0;i<M;i++){
+           fscanf(f,"%d ",&day);
+           sum+=day;
+           A[i]=day;
+           B[i]=sum; 
+       }
+    }
     
 }
+
