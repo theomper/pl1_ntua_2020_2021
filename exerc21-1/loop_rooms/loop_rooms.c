643499bc0ee9f99fc47377b24e85f31b3e0313b4
theomper <theo.mper@gmail.com>
2021-04-10 01:02:03 +0300

    loop_rooms.c initialization

---
+++ exerc21-1/loop_rooms/loop_rooms.c
@@ -0,0 +1,17 @@
+// FILENAME : loop_rooms.c
+// DESCRIPTION : exerc21-1 / pl1-ntua
+// AUTHOR : Theo Mper & Alex Tsaf
+// COMPILE COMMAND : gcc -std=c99 -Wall -Werror -O3 -o loop_rooms loop_rooms.c
+// EXECUTE COMMAND : ./loop_rooms input.file
+// EXTERNAL USED CODE :
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Input reading, memory allocation and function calling
+int main(int argc,char *argv[]) {
+
+
+    return (0);
+}
