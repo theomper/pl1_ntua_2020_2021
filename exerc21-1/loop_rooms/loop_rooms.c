746e7e05d516fa55fd011fc54c6e27932fdf137a
theomper <theo.mper@gmail.com>
2021-04-10 02:18:59 +0300

    loop_rooms input reading

--- exerc21-1/loop_rooms/loop_rooms.c
+++ exerc21-1/loop_rooms/loop_rooms.c
@@ -12,6 +12,53 @@
 // Input reading, memory allocation and function calling
 int main(int argc,char *argv[]) {
 
+    // Declarations
+    FILE *fp;
+    int i, j, rows = 0, columns = 0;
+
+    //Unsigned char **grid;
+    int ret = 0;
+    unsigned char grid[1002][1002];
+
+    // File opening, input reading and memory allocation
+    fp = fopen(argv[1], "r");
+    if (fp == NULL) {
+        printf("Error Reading File\n");
+    }
+
+       // Read how many are the rows
+    ret = fscanf(fp, "%d", &rows);
+    if (ret == 0) {
+        printf("No rows!\n");
+    }
+    printf("Rows: %d\n", rows);
+
+    ret = fscanf(fp, "%d", &columns);
+    if (ret == 0) {
+        printf("No columns!\n");
+    }
+    printf("Columns: %d\n", columns);
+
+    fgetc(fp);
+
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < columns; j++) {
+            // read grid and insert Qnodes for matter ('+') or anti-matter ('-')
+            ret = fscanf(fp, "%c", &grid[i][j]);
+        }
+        fgetc(fp);
+    }
+
+    // Close input file
+    fclose(fp);
+
+    for (i = 0; i < rows + 1; i++) {
+        for (j = 0; j < columns + 1; j++) {
+            printf("%c", grid[i][j]);
+        }
+        printf("\n");
+    }
+
 
     return (0);
 }
