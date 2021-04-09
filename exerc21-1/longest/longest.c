f361474041f3592bd540458645563ff4c84f8b06
theomper <theo.mper@gmail.com>
2021-04-10 00:49:55 +0300

    Added some extra commentation

--- exerc21-1/longest/longest.c
+++ exerc21-1/longest/longest.c
@@ -14,12 +14,11 @@
 #include <math.h>
 #include <limits.h>
 
-// returns only -1, 0 or 1 depensing the comparison
+// Returns only -1, 0 or 1 depending the comparison
 int compare ( const void *pa, const void *pb ) {
     const long int *a = *(const long int **)pa;
     const long int *b = *(const long int **)pb;
     if(a[0] == b[0]) {
-        // return a[1] - b[1];
         if ((a[1] - b[1]) < 0) {
             return -1;
         }
@@ -31,7 +30,6 @@ int compare ( const void *pa, const void *pb ) {
         }
     }
     else {
-        // return a[0] - b[0];
         if ((a[0] - b[0]) < 0) {
             return -1;
         }
@@ -73,7 +71,7 @@ int find_ind(long int **presum, int n, int val) {
     return ans;
 }
 
-/*Function to find minimum of x and y*/
+// Function to find minimum of x and y
 long int min(long int x,long int y) {
     return y ^ ((x ^ y) & -(x < y));
 }
@@ -86,7 +84,7 @@ long int max(long int x,long int y) {
 // Function to find the length of the longest subarray with average >= x
 long int longest_subarray(long int number_of_days, int number_of_hospitals, long int *days) {
 
-    // declarations
+    // Ddeclarations
     long int i, maxlen, sum, *min_indx;
 
     // Update array by subtracting x from each element.
@@ -110,14 +108,14 @@ long int longest_subarray(long int number_of_days, int number_of_hospitals, long
     // To store current value of prefix sum.
     sum = 0;
 
-    // To store minimum index value in range 0..i of preSum vector.
+    // To store minimum index value in range 0..i of presum vector.
     min_indx = malloc(sizeof(long int) * number_of_days);
     if (!min_indx) { /* If N == 0 after the call to malloc, allocation failed for some reason */
         perror("Error allocating memory for N");
         abort();
     }
 
-    // Insert values in preSum vector.
+    // Insert values in presum vector.
     for (i = 0; i < number_of_days; i++) {
         sum = sum + days[i];
         presum[i][0] = sum;
@@ -126,7 +124,7 @@ long int longest_subarray(long int number_of_days, int number_of_hospitals, long
 
     qsort(presum, number_of_days, sizeof presum[0], compare);
 
-    // Update minInd array.
+    // Update min_ind array.
     min_indx[0] = presum[0][1];
 
     for (i = 1; i < number_of_days; i++) {
@@ -137,16 +135,15 @@ long int longest_subarray(long int number_of_days, int number_of_hospitals, long
     for (i = 0; i < number_of_days; i++) {
         sum = sum + days[i];
   
-        // If sum is greater than k, then answer
-        // is i+1.
+        // If sum is greater than number_of_hospitals, then answer is i+1.
         if (sum > number_of_hospitals) {
             maxlen = i + 1;
         }
 
-        // If the sum is less than or equal to k, then
+        // If the sum is less than or equal to number_of_hospitals, then
         // find if there is a prefix array having sum
         // that needs to be added to the current sum to
-        // make its value greater than k. If yes, then
+        // make its value greater than number_of_hospitals. If yes, then
         // compare the length of updated subarray with
         // maximum length found so far.
         else {
@@ -157,63 +154,66 @@ long int longest_subarray(long int number_of_days, int number_of_hospitals, long
         }
     }
 
+    // Free the memory
     free(*presum);
-  
+
     return maxlen;
 }
 
-// input reading, memory allocation and function calling
+// Input reading, memory allocation and function calling
 int main(int argc,char *argv[]) {
 
-    // declarations
+    // Declarations
     FILE *fp;
     long int i, ret, number_of_hospitals, number_of_days, *days, result;
 
-    // file opening, input reading and memory allocation
+    // File opening, input reading and memory allocation
     fp = fopen(argv[1], "r");
     if (fp == NULL) {
         printf("Error Reading File\n");
     }
 
-    // read how many are the numbers
+    // Read how many are the numbers
     ret = fscanf(fp, "%li", &number_of_days);
     if (ret == 0) {
         printf("No numbers!\n");
     }
 
-    // malloc for N times memory slots
+    // Malloc for N times memory slots
     days = malloc(sizeof(long int) * number_of_days);
     if (!days) { /* If N == 0 after the call to malloc, allocation failed for some reason */
         perror("Error allocating memory for N");
         abort();
     }
 
-    // at this point, we know that N and K points to a valid block of memory.
-    // however this memory may contains garbage.
+    // At this point, we know that N and K points to a valid block of memory.
+    // However this memory may contains garbage.
     memset(days, 0, sizeof(long int) * number_of_days);
-    // now our arrays contains all zeroes
+    // Now our arrays contains all zeroes
 
     ret = fscanf(fp, "%li", &number_of_hospitals);
     if (ret == 0) {
         printf("No hospitals!\n");
     }
 
-    // read numbers and powers
+    // Read numbers and powers
     for (i = 0; i < number_of_days; i++) {
         ret = fscanf(fp, "%li", &days[i]);
     }
 
-    // close input file
+    // Close input file
     fclose(fp);
 
-    // call find solution function
+    // Multiplying input by -1
     for (i = 0; i < number_of_days; i++) {
         days[i] = -days[i];
     }
 
+    // Call find solution function
     result = longest_subarray(number_of_days, number_of_hospitals, days);
     printf("%li\n", result);
 
+    // Free the memory
     free(days);
 
     return(0);
