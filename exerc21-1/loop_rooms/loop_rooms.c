7eed73173990cff28597ce65059e481a06c3ceb3
theomper <theo.mper@gmail.com>
2021-04-12 17:02:12 +0300

    Working loop_rooms. Missing explanatory comments

--- exerc21-1/loop_rooms/loop_rooms.c
+++ exerc21-1/loop_rooms/loop_rooms.c
@@ -3,11 +3,56 @@
 // AUTHOR : Theo Mper & Alex Tsaf
 // COMPILE COMMAND : gcc -std=c99 -Wall -Werror -O3 -o loop_rooms loop_rooms.c
 // EXECUTE COMMAND : ./loop_rooms input.file
-// EXTERNAL USED CODE :
+// EXTERNAL USED CODE : -
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+unsigned char rooms[1002][1002];
+int visited[1002][1002], fairness[1002][1002];
+
+int fairbox(int ix, int jx, int rows, int columns) {
+
+    if (visited[ix][jx] == 1 && fairness[ix][jx] == -1) {
+        fairness[ix][jx] = 0;
+        return 0;
+    }
+      if (fairness[ix][jx] != -1) {
+        return fairness[ix][jx];
+    }
+
+    if (((ix == 1) && rooms[ix][jx] == 'U') 
+        || ((ix == rows) && rooms[ix][jx] == 'D') 
+        || ((jx == 1) && rooms[ix][jx] == 'L') 
+        || ((jx == columns) && rooms[ix][jx] == 'R')) {
+        visited[ix][jx] = 1;
+        fairness[ix][jx] = 1;
+        return 1;
+    }
+  
+    switch (rooms[ix][jx]) {
+        case 'U':visited[ix][jx] = 1;
+        	fairness[ix][jx] = fairbox(ix - 1, jx, rows, columns);
+                    return fairness[ix - 1][jx];
+                    break;
+        case 'D':  visited[ix][jx] = 1;
+        	 fairness[ix][jx] = fairbox(ix + 1, jx, rows, columns);
+                    return fairness[ix + 1][jx];
+                    break;
+        case 'L':   visited[ix][jx] = 1;
+        	fairness[ix][jx] = fairbox(ix, jx - 1, rows, columns);
+                    return fairness[ix][jx - 1];
+                    break;
+        case 'R':   visited[ix][jx] = 1;
+		     fairness[ix][jx] = fairbox(ix, jx + 1, rows, columns);
+                    return fairness[ix][jx + 1];
+                    break;
+        default: break;
+    }
+
+    return fairness[ix][jx];
+}
 
 // Input reading, memory allocation and function calling
 int main(int argc,char *argv[]) {
@@ -16,9 +61,9 @@ int main(int argc,char *argv[]) {
     FILE *fp;
     int i, j, rows = 0, columns = 0;
 
-    //Unsigned char **grid;
+    //Unsigned char **rooms;
     int ret = 0;
-    unsigned char grid[1002][1002];
+    // unsigned char rooms[1000][1000], visited[1000][1000], fairness[1000][1000];
 
     // File opening, input reading and memory allocation
     fp = fopen(argv[1], "r");
@@ -26,25 +71,33 @@ int main(int argc,char *argv[]) {
         printf("Error Reading File\n");
     }
 
-       // Read how many are the rows
+    // Read how many are the rows
     ret = fscanf(fp, "%d", &rows);
     if (ret == 0) {
         printf("No rows!\n");
     }
-    printf("Rows: %d\n", rows);
 
     ret = fscanf(fp, "%d", &columns);
     if (ret == 0) {
         printf("No columns!\n");
     }
-    printf("Columns: %d\n", columns);
 
-    fgetc(fp);
+    char c;
+    ret=fscanf(fp,"%c",&c);
 
-    for (i = 0; i < rows; i++) {
-        for (j = 0; j < columns; j++) {
-            // read grid and insert Qnodes for matter ('+') or anti-matter ('-')
-            ret = fscanf(fp, "%c", &grid[i][j]);
+    for (i = 0; i <= rows + 1; i++) {
+        for (j = 0; j <= columns + 1; j++) {
+            if (i == 0 || j == 0 || i == rows + 1 || j == columns + 1) {
+                rooms[i][j] = 'X';
+            }
+        }
+    }
+
+    for (i = 1; i < rows + 1; i++) {
+        for (j = 1; j < columns + 1; j++) {
+            ret = fscanf(fp, "%c", &rooms[i][j]);
+            visited[i][j] = 0;
+            fairness[i][j] = -1;
         }
         fgetc(fp);
     }
@@ -52,13 +105,45 @@ int main(int argc,char *argv[]) {
     // Close input file
     fclose(fp);
 
-    for (i = 0; i < rows + 1; i++) {
-        for (j = 0; j < columns + 1; j++) {
-            printf("%c", grid[i][j]);
+    //for (i = 0; i < rows + 2; i++) {
+    //    for (j = 0; j < columns + 2; j++) {
+    //       printf("%c", rooms[i][j]);
+    //    }
+    //    printf("\n");
+   // }
+
+    // Find fairbox
+     for (i = 1; i < rows + 1; i++) {
+        for (j = 1; j < columns + 1; j++) {
+            fairbox(i, j, rows, columns);
         }
-        printf("\n");
     }
 
+    int counter = 0;
+    // Counting the unfair rooms
+    for (i = 1; i < rows + 1; i++) {
+        for (j = 1; j < columns + 1; j++) {
+            if(fairness[i][j]==0) counter++;
+        }
+    }
+    printf("%d\n", counter);
+    // for (i = 1; i < rows; i++) {
+    //         printf("%d%d %c",i,j, rooms[i][j]);
+    //         }
+    //   printf("\n");
+    // }
+    // for (i = 0; i < rows; i++) {
+    //     for (j = 0; j < columns; j++) {
+    //         printf("%d", visited[i][j]);
+    //     }
+    //     printf("\n");
+    // }
+    // for (i = 1; i < rows+1; i++) {
+    //     for (j = 1; j < columns+1; j++) {
+    //         printf("%d", fairness[i][j]);
+    //     }
+    //     printf("\n");
+    // }
 
     return (0);
 }
