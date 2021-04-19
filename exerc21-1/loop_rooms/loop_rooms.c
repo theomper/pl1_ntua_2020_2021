b4c200f69e4cda52cfbb5fcd37a8f8b0684b8b22
theomper <theo.mper@gmail.com>
2021-04-19 13:50:26 +0300

    cleaned trash code of loop_rooms

--- exerc21-1/loop_rooms/loop_rooms.c
+++ exerc21-1/loop_rooms/loop_rooms.c
@@ -9,8 +9,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-unsigned char rooms[1002][1002];
-int visited[1002][1002], fairness[1002][1002];
+unsigned char rooms[1000][1000];
+int visited[1000][1000], fairness[1000][1000];
 
 int fairbox(int ix, int jx, int rows, int columns) {
 
@@ -22,10 +22,10 @@ int fairbox(int ix, int jx, int rows, int columns) {
         return fairness[ix][jx];
     }
 
-    if (((ix == 1) && rooms[ix][jx] == 'U') 
-        || ((ix == rows) && rooms[ix][jx] == 'D') 
-        || ((jx == 1) && rooms[ix][jx] == 'L') 
-        || ((jx == columns) && rooms[ix][jx] == 'R')) {
+    if (((ix == 0) && rooms[ix][jx] == 'U') 
+        || ((ix == rows - 1) && rooms[ix][jx] == 'D') 
+        || ((jx == 0) && rooms[ix][jx] == 'L') 
+        || ((jx == columns - 1) && rooms[ix][jx] == 'R')) {
         visited[ix][jx] = 1;
         fairness[ix][jx] = 1;
         return 1;
@@ -63,7 +63,6 @@ int main(int argc,char *argv[]) {
 
     //Unsigned char **rooms;
     int ret = 0;
-    // unsigned char rooms[1000][1000], visited[1000][1000], fairness[1000][1000];
 
     // File opening, input reading and memory allocation
     fp = fopen(argv[1], "r");
@@ -85,16 +84,8 @@ int main(int argc,char *argv[]) {
     char c;
     ret=fscanf(fp,"%c",&c);
 
-    for (i = 0; i <= rows + 1; i++) {
-        for (j = 0; j <= columns + 1; j++) {
-            if (i == 0 || j == 0 || i == rows + 1 || j == columns + 1) {
-                rooms[i][j] = 'X';
-            }
-        }
-    }
-
-    for (i = 1; i < rows + 1; i++) {
-        for (j = 1; j < columns + 1; j++) {
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < columns; j++) {
             ret = fscanf(fp, "%c", &rooms[i][j]);
             visited[i][j] = 0;
             fairness[i][j] = -1;
@@ -105,45 +96,21 @@ int main(int argc,char *argv[]) {
     // Close input file
     fclose(fp);
 
-    //for (i = 0; i < rows + 2; i++) {
-    //    for (j = 0; j < columns + 2; j++) {
-    //       printf("%c", rooms[i][j]);
-    //    }
-    //    printf("\n");
-   // }
-
     // Find fairbox
-     for (i = 1; i < rows + 1; i++) {
-        for (j = 1; j < columns + 1; j++) {
+     for (i = 0; i < rows; i++) {
+        for (j = 0; j < columns; j++) {
             fairbox(i, j, rows, columns);
         }
     }
 
     int counter = 0;
     // Counting the unfair rooms
-    for (i = 1; i < rows + 1; i++) {
-        for (j = 1; j < columns + 1; j++) {
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < columns; j++) {
             if(fairness[i][j]==0) counter++;
         }
     }
     printf("%d\n", counter);
-    // for (i = 1; i < rows; i++) {
-    //         printf("%d%d %c",i,j, rooms[i][j]);
-    //         }
-    //   printf("\n");
-    // }
-    // for (i = 0; i < rows; i++) {
-    //     for (j = 0; j < columns; j++) {
-    //         printf("%d", visited[i][j]);
-    //     }
-    //     printf("\n");
-    // }
-    // for (i = 1; i < rows+1; i++) {
-    //     for (j = 1; j < columns+1; j++) {
-    //         printf("%d", fairness[i][j]);
-    //     }
-    //     printf("\n");
-    // }
 
     return (0);
 }
