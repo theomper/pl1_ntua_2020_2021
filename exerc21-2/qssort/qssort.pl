655c98643dcb4554fccbeb14a0b67846a277ea15
alexece07 <atsaf98@gmail.com>
2021-05-25 20:40:08 +0300

    QS Sort in Prolog potentially ready 2.0

--- exerc21-2/qssort/qssort.pl
+++ exerc21-2/qssort/qssort.pl
@@ -1,4 +1,3 @@
-
 read_input(File, N, C) :-
     open(File, read, Stream),
     read_line(Stream, N),
@@ -20,28 +19,14 @@ final(config(Queue, Stack), File) :-
     Stack = [].
 
 
-move(config([H|Tail],S1),config(L2,S2), 'Q') :-
+move(config([H|Tail],S1), config(L2,S2), 'Q') :-
     L2 = Tail,
     S2 = [H|S1].
-move(config(L1,[Elem|[]]), config(L2, S2), 'S') :-
-    append(L1, [Elem], L2),
-    S2 = [].
 move(config(L1, S1), config(L2, S2), 'S') :-
     (S1 = [Elem|_] -> 
         append(L1, [Elem], L2),
         append([Elem], S2, S1)).
 
-% move(L1, S1, L2, S2) :-
-%     (L1 = [] -> false
-%     ; L1 =[H|Tail]-> L2 = Tail, S2 = [H|S1]).
-
-% move(L1, S1, L2, S2) :-
-%     (S1 = []-> false
-%      ; S1 = [Elem|[]] -> L2 = [L1|Elem], S2 = []
-%      ; S1 = [Elem|_] -> L2 = [L1|Elem], pop(Elem, S1, S2)).
-
-pop(E, [E|Es], Es).
-push(E, Es, [E|Es]).
 
 solve(Conf, [], Final) :- Conf = Final.
 solve(Conf, [Move|Moves], Final) :-
